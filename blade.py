8d6e2d53718d8e5a0baddc435993dcc95069f03a
Zoe McCarthy <zoemccarthy12@gmail.com>
2013-04-20 06:51:40 -0500

    lots of unit tests, several blade products implemented

--- blade.py
+++ blade.py
@@ -3,10 +3,10 @@ import scipy.linalg as la
 
 
 class Blade(object):
-    def __init__(self, B=None, orthonormal=False, s=None, gpu=False, tol=1e8):
+    def __init__(self, blade=None, orthonormal=False, s=None, gpu=False, tol=1e-8):
         """
         Inputs:
-        B           : n by k np array that is the list of k vectors of 
+        blade       : n by F np array that is the list of k vectors of 
                       dimension n to be spanned
         orthonormal : specify whether or not the input B is already orthonormal
         s           : scale factor multiplier (will be multiplied to 
@@ -16,32 +16,71 @@ class Blade(object):
         """
         self.gpu = gpu # gpu not supported yet -- stay tuned!
 
-        if B is None:
-            self.k = 0
-            self.s = s if s is not None else 1
-            self.B = np.array([])
+        if blade is None:
+            self.s = 0
         else:
-            n, k = B.shape
+            try:
+                shape = blade.shape
+            except AttributeError: #try scalar?
+                blade = np.array([[blade]])
+                shape = blade.shape
+            except:
+                raise
+            if len(shape) == 1: #only one index, make column vector
+                blade = np.array([blade]).T
+            n, k = blade.shape
+            self.k = k
             if orthonormal:
-                self.k = k
                 self.s = s if s is not None else 1
-                self.B = B
+                self.blade = blade
             else:
                 # QR factorization
-                Q, R, P = la.qr(B, mode='economic', pivoting=True)
+                Q, R, P = la.qr(blade, mode='economic', pivoting=True)
+                print Q, R, P
+                print np.dot(Q.T, Q)
+                print k
+                print R[k - 1, k - 1]
                 if abs(R[k - 1, k - 1]) >= tol:
-                   self.k = k
                    self.s = np.prod(np.diag(R))
                    if s is not None: self.s *= s
-                   self.B = Q
+                   self.blade = np.copy(Q)
+                   print 'self.s: ', self.s
+                   print 'self.blade: ', self.blade
                 else:
-                   self.k = 0
-                   self.s = s if s is not None else 1
-                   self.B = np.array([])
-        if np.allclose(s, 0.0):
+                   self.s = 0
+        if np.allclose(self.s, 0): # reset to null blade
             self.k = 0
             self.s = 0
-            self.B = np.array([])
+            self.blade = np.array([])
+
+def outer(blade1, blade2):
+    """ Calculates the blade outer product of blade1 ^ blade2 """
+    # TODO: implement operator overloading for ^
+    pass
+
+def reverse(blade):
+    """ Calculates the reverse of k-blade blade  """
+    blade.s *= pow(-1, (blade.k * (blade.k - 1)) / 2)
+    return blade
+
+def involution(blade):
+    """ Calculates the grade involution of k-blade blade  """
+    blade.s *= pow(-1, blade.k)
+    return blade
+
+def inner(blade1, blade2):
+    """ Calculates the blade inner product of blade1 \circdot blade2 """
+    k = blade1.k
+    scale = (-1)**((k * (k - 1)) / 2) * \
+            la.det(np.dot(blade1.blade.T, blade2.blade))
+    C = Blade(blade=1, s=scale)
+    return C
+    
+
+
+
+
+    
 
                 
 
--- tests.py
+++ tests.py
@@ -1,28 +1,146 @@
 import numpy as np
 import nose
 from blade import Blade
-# -------- #  Blade tests
+import blade as bd
+# -------- #  BEGIN Blade tests
 
-# -------- #  Blade.__init__() tests
+# -------- #  SUBBEGIN Blade.__init__() tests
 
 def testBladeInit():
-    " test all possible permutations of inputting s and common things "
+    pass
+def testBladeInitNull():
+    " Testing null blade "
     B = Blade()
-    assert np.allclose(B.B, np.array([]))
+    assert B.blade.shape == (0,)
+    assert np.allclose(B.s, 0)
+    assert B.k == 0
 
-    A = np.array([[1, 0], [0, 1]])
+    B = Blade(s=0)
+    assert B.blade.shape == (0,)
+    assert np.allclose(B.s, 0)
+    assert B.k == 0
+
+def testBladeInitScalar():
+    " Testing scalar blade "
+    B = Blade(1)
+    assert B.blade.shape == (1, 1)
+    assert np.allclose(B.s, 1)
+    assert B.k == 1
+
+    B = Blade(1, s=2)
+    assert B.blade.shape == (1, 1)
+    assert np.allclose(B.s, 2)
+    assert B.k == 1
+
+    B = Blade(2)
+    assert B.blade.shape == (1, 1)
+    assert np.allclose(B.s, 2)
+    assert B.k == 1
+
+    B = Blade(2, s=2)
+    assert B.blade.shape == (1, 1)
+    assert np.allclose(B.s, 4)
+    assert B.k == 1
+
+def testBladeInitOrthonormalQr():
+    " Testing orthonormal and qr "
+    # testing orthonormal
+    A = np.array([[1, 0], [0, 1], [0, 0]])
+
     B = Blade(A, orthonormal=True)
-    assert np.allclose(B.B, A)
+    assert np.allclose(B.blade, A)
     assert np.allclose(B.s, 1.0)
 
     B = Blade(A, orthonormal=True, s=2.0)
-    assert np.allclose(B.B, A)
+    assert np.allclose(B.blade, A)
     assert np.allclose(B.s, 2.0)
 
     B = Blade(A, orthonormal=True, s=0.0)
-    assert np.allclose(B.B, np.array([]))
+    assert B.blade.shape == (0,)
     assert np.allclose(B.s, 0)
+    assert B.k == 0
+
+    # testing qr
+    B = Blade(A)
+    print 'B.blade: ', B.blade
+    assert np.allclose(np.dot(B.blade.T, B.blade), np.eye(2))
+    assert np.allclose(B.s, 1)
+
+    B = Blade(A, s=2.0)
+    assert np.allclose(np.dot(B.blade.T, B.blade), np.eye(2))
+    assert np.allclose(B.s, 2.0)
+
+def testBladeInitQrScaling():
+    # testing qr scaling
+    A = np.array([[2, 0], [0, 1], [0, 0]])
 
     B = Blade(A)
-    assert np.allclose(B.B, A)
-    assert np.allclose(B.s, 1)
+    assert np.allclose(np.dot(B.blade.T, B.blade), np.eye(2))
+    assert np.allclose(B.s, 2.0)
+
+    B = Blade(A, s=2.0)
+    assert np.allclose(np.dot(B.blade.T, B.blade), np.eye(2))
+    assert np.allclose(B.s, 4.0)
+
+def testBladeInitRankDeficient():
+    # testing rank deficient
+    A = np.array([[1, 0], [1, 0], [0, 0]])
+
+    B = Blade(A)
+    assert B.blade.shape == (0,)
+    assert np.allclose(B.s, 0)
+    assert B.k == 0
+
+    B = Blade(A, s=2.0)
+    assert B.blade.shape == (0,)
+    assert np.allclose(B.s, 0)
+    assert B.k == 0
+
+def testBladeInitColumnVector():
+    # testing column vector
+    c = np.array([[2], [0], [0]])
+
+    B = Blade(c)
+    assert np.allclose(np.dot(B.blade.T, B.blade), np.eye(1))
+    assert np.allclose(B.s, 2.0)
+    assert B.k == 1
+
+    B = Blade(c, s=2.0)
+    assert np.allclose(np.dot(B.blade.T, B.blade), np.eye(1))
+    assert np.allclose(B.s, 4.0)
+    assert B.k == 1
+
+
+def testBladeInitOneIndexVector():
+    # testing one-index (row-ish) vector
+    c = np.array([2, 0, 0])
+
+    B = Blade(c)
+    assert np.allclose(np.dot(B.blade.T, B.blade), np.eye(1))
+    assert np.allclose(B.s, 2.0)
+    assert B.k == 1
+
+def testBladeReverse():
+    for i in range(1, 5):
+        B = Blade(np.eye(i))
+        assert np.allclose(B.s, (-1)**(B.k * (B.k - 1) / 2) * bd.reverse(B).s)
+        assert B.k == i
+
+
+def testBladeInvolutio():
+    for i in range(1, 5):
+        B = Blade(np.eye(i))
+        assert np.allclose(B.s, (-1)**B.k * bd.involution(B).s)
+        assert B.k == i
+
+def testBladeInnerProduct():
+    for i in range(1, 5):
+        B = Blade(np.eye(i + 2, i))
+        assert np.allclose(B.s, (-1)**B.k * bd.involution(B).s)
+        assert B.k == i
+    
+
+
+if __name__ == "__main__":
+    nose.main()
+
