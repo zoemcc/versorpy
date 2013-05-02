Outer finished, tested, fixed column vector blade input scaling, new test.

blade.py
@@ -50,12 +50,21 @@ class Blade(object):
                 shape = blade.shape
             n, k = shape
             self.n, self.k = n, k
-            if k == 0:
+            if k == 0: # scalar
                 self.blade = blade
-            elif orthonormal:
+            elif n < k: # too many columns, can't be a k-dim subspace of R^n
+                self.s = 0
+            elif orthonormal: # no qr/rescaling required
                 self.s = s if s is not None else 1
                 self.blade = blade
-            else:
+            elif k == 1: # just a vector
+                mag = la.norm(blade)
+                if abs(mag) >= tol:
+                    self.s = s * mag if s is not None else mag
+                    self.blade = blade / mag
+                else:
+                    self.s = 0
+            else: # full blade case
                 # QR factorization
                 Q, R, P = la.qr(blade, mode='economic', pivoting=True)
                 print Q, R, P
@@ -76,23 +85,32 @@ class Blade(object):
             self.s = 0
             self.blade = np.array([[]])
 
-def outer(blade1, blade2):
+def outer(blade1, blade2, tol=1e-10):
     """ Calculates the blade outer product of blade1 ^ blade2 """
     # TODO: implement operator overloading for ^
+    print 'blade1: ', blade1.blade
+    print 'blade2: ', blade2.blade
     k, l = blade1.k, blade2.k
-    if k == 0:
+    if k == 0 and l == 0: # scalar/scalar
+        return Blade(1, s = blade1.s * blade2.s)
+    elif k == 0: # scalar/blade
         return Blade(blade2.blade, orthonormal=True, s=blade1.s * blade2.s)
-    elif l == 0:
+    elif l == 0: # blade/scalar
         return Blade(blade1.blade, orthonormal=True, s=blade1.s * blade2.s)
-    elif k + l > blade1.blade.shape[0]:
+    elif k + l > blade1.blade.shape[0]: # too many factors
         return Blade()
-    else:
+    else: # full calculation
         O = np.concatenate((blade1.blade, blade2.blade), axis=1)
         U, s, Vh = la.svd(O)
-        # TODO: finish this case
-
-
-
+        if abs(s[k + l - 1]) < tol:
+            return Blade()
+        else:
+            matrixSpan = U[:, :k + l]
+            scale = blade1.s * blade2.s * la.det(np.dot(O.T, matrixSpan))
+            print 'Span: ', matrixSpan
+            print 'O: ', O
+            print 'scale: ', scale
+            return Blade(blade=matrixSpan, orthonormal=True, s=scale)
 
 def inverse(blade):
     """ Calculates the inverse of k-blade blade  """
@@ -176,6 +194,9 @@ def inner(blade1, blade2):
                 la.det(np.dot(blade1.blade.T, blade2.blade))
         C = Blade(1, s=scale)
         return C
+
+def pseudoScalar(n):
+    return Blade(blade=np.eye(n, n))
     
 
 

tests.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.linalg as la
 import nose
 from blade import Blade
 import blade as bd
@@ -102,15 +103,27 @@ def testBladeInitRankDeficient():
     assert np.allclose(B.s, 0)
     assert B.k == 0
 
+    # too many columns
+    C = np.array([[1, 0, 1], [0, 1, 0]])
+    B2 = Blade(C)
+    assert B2.blade.shape == (1, 0)
+    assert np.allclose(B2.s, 0)
+    assert B2.k == 0
+
+
 def testBladeInitColumnVector():
     # testing column vector
     c = np.array([[2], [0], [0]])
     B = Blade(c)
+    print 's: ', B.s
+    print 'blade: ', B.blade
     assert np.allclose(np.dot(B.blade.T, B.blade), np.eye(1))
     assert np.allclose(B.s, 2.0)
     assert B.k == 1
 
     B = Blade(c, s=2.0)
+    print 's: ', B.s
+    print 'blade: ', B.blade
     assert np.allclose(np.dot(B.blade.T, B.blade), np.eye(1))
     assert np.allclose(B.s, 4.0)
     assert B.k == 1
@@ -140,7 +153,7 @@ def testBladeInvolutio():
         assert np.allclose(B.s, (-1)**B.k * bd.involution(B).s)
         assert np.allclose(B.s, (-1)**B.k * bd.involutionScaling(B))
 
-def testBladeInnerProductSign():
+def testBladeInnerSign():
     # sign
     for i in range(1, 5):
         B = Blade(np.eye(i, i))
@@ -148,7 +161,7 @@ def testBladeInnerProductSign():
         assert np.allclose((-1)**((i * (i - 1)) / 2), bd.inner(B, B).s), 'value'
         assert bd.inner(B, B).blade.shape == (1, 0), 'shape'
 
-def testBladeInnerProductScale():
+def testBladeInnerScale():
     # scale
     A = np.array([[2, 0], [0, 1]])
     print 'A.T A : ', np.dot(A.T, A)
@@ -164,11 +177,20 @@ def testBladeInnerProductScale():
     print 'B inner B: ', bd.inner(B, B).s
     assert np.allclose(-4, bd.inner(B, B).s)
 
-def testBladeInnerProductNonGradeMatch():
+def testBladeInnerNonGradeMatch():
     B1 = Blade(np.eye(2))
     B2 = Blade(np.eye(1))
     print 'B inner B: ', bd.inner(B1, B2).s
     assert np.allclose(0, bd.inner(B1, B2).s)
+
+def testBladeInnerCommutativity():
+    # commutative
+    for i in range(1, 5):
+        B1 = Blade(np.eye(i, i))
+        B2 = Blade(-np.eye(i, i))
+        firstOrdering = bd.inner(B1, B2)
+        secondOrdering = bd.inner(B2, B1)
+        assert np.allclose(firstOrdering.s, secondOrdering.s), (firstOrdering.s, secondOrdering.s)
 
 def testBladeInverse():
     B1 = Blade(np.eye(2))
@@ -202,6 +224,108 @@ def testBladeCopy():
 
 
 
+def testBladeOuterSign():
+    # sign/antisymmetric
+    B1 = Blade(np.array([1, 0]))
+    B2 = Blade(np.array([0, 1]))
+    result1 = bd.outer(B1, B2)
+    print 'B1 outer B2 s: ', result1.s
+    print 'B1 outer B2 blade: ', result1.blade
+    assert la.det(result1.blade) > 0, la.det(result1.blade)
+    assert result1.blade.shape == (2, 2), result1.blade.shape
+    assert np.allclose(np.eye(2), np.dot(result1.blade.T, result1.blade))
+    assert np.allclose(bd.inner(result1, result1).s, -1), bd.inner(result1, result1).s
+
+    result2 = bd.outer(B2, B1)
+    print 'B2 outer B1 s: ', result2.s
+    print 'B2 outer B1 blade: ', result2.blade
+    assert la.det(result2.blade) < 0, la.det(result2.blade)
+    assert result2.blade.shape == (2, 2), result2.blade.shape
+    assert np.allclose(np.eye(2), np.dot(result2.blade.T, result2.blade))
+    assert np.allclose(bd.inner(result2, result2).s, -1), bd.inner(result2, result2).s
+    
+    # result1 == inverse(result2)
+    assert bd.inner(result1, result2).s > 0, bd.inner(result1, result2).s
+
+
+def testBladeOuterScalar():
+    # scalar to blade/ scalar to scalar
+    B1 = Blade(np.eye(2))
+    B2 = Blade(1, s=2)
+
+    result1 = bd.outer(B1, B2)
+    print 'B1 outer B2 s: ', result1.s
+    assert np.allclose(2, result1.s), 'value'
+    assert result1.blade.shape == (2, 2), 'shape'
+
+    result2 = bd.outer(B2, B1)
+    print 'B2 outer B1 s: ', result2.s
+    assert np.allclose(2, result2.s), 'value'
+    assert result2.blade.shape == (2, 2), 'shape'
+
+    result3 = bd.outer(B2, B2)
+    print 'B2 outer B2 s: ', result3.s
+    assert np.allclose(result3.s, 4)
+
+
+def testBladeOuterScaleMultplicative():
+    # scale (just multiplicative scaling)
+    B1 = Blade(np.array([2, 0]))
+    B2 = Blade(np.array([0, 1]))
+    result1 = bd.outer(B1, B2)
+    print 'B1 outer B2 s: ', result1.s
+    print 'B1 outer B2 blade: ', result1.blade
+    assert la.det(result1.blade) > 0, la.det(result1.blade)
+    assert result1.blade.shape == (2, 2), result1.blade.shape
+    assert np.allclose(np.eye(2), np.dot(result1.blade.T, result1.blade))
+    assert np.allclose(bd.inner(result1, result1).s, -4), bd.inner(result1, result1).s
+
+    result2 = bd.outer(B2, B1)
+    print 'B2 outer B1 s: ', result2.s
+    print 'B2 outer B1 blade: ', result2.blade
+    assert la.det(result2.blade) < 0, la.det(result2.blade)
+    assert result2.blade.shape == (2, 2), result2.blade.shape
+    assert np.allclose(np.eye(2), np.dot(result2.blade.T, result2.blade))
+    assert np.allclose(bd.inner(result2, result2).s, -4), bd.inner(result2, result2).s
+    
+    # result1 == inverse(result2)
+    assert np.allclose(bd.inner(result1, result2).s, 4), bd.inner(result1, result2).s
+
+def testBladeOuterScaleParallelopiped():
+    # scale (outer(B1, B2).s == 'area spanned by parallelopiped between B1, B2')
+    B1 = Blade(np.array([1, 0]))
+    B2 = Blade(np.array([1, 1])) # should have same area as [0, 1] when outered with B1
+    result1 = bd.outer(B1, B2)
+    print 'B1 outer B2 s: ', result1.s
+    print 'B1 outer B2 blade: ', result1.blade
+    assert la.det(result1.blade) > 0, la.det(result1.blade)
+    assert result1.blade.shape == (2, 2), result1.blade.shape
+    assert np.allclose(np.eye(2), np.dot(result1.blade.T, result1.blade))
+    assert np.allclose(bd.inner(result1, result1).s, -1), bd.inner(result1, result1).s
+
+    result2 = bd.outer(B2, B1)
+    print 'B2 outer B1 s: ', result2.s
+    print 'B2 outer B1 blade: ', result2.blade
+    assert la.det(result2.blade) < 0, la.det(result2.blade)
+    assert result2.blade.shape == (2, 2), result2.blade.shape
+    assert np.allclose(np.eye(2), np.dot(result2.blade.T, result2.blade))
+    assert np.allclose(bd.inner(result2, result2).s, -1), bd.inner(result2, result2).s
+    
+    # result1 == inverse(result2)
+    assert np.allclose(bd.inner(result1, result2).s, 1), bd.inner(result1, result2).s
+
+def testBladeOuterSameSubspace():
+    B1 = Blade(np.eye(2))
+    B2 = Blade(np.eye(2)[:, 0])
+    result = bd.outer(B1, B2)
+    print 'B1 outer B2: ', result.s
+    assert np.allclose(0, result.s)
+
+    B1 = Blade(np.eye(2))
+    B2 = Blade(np.eye(2))
+    result = bd.outer(B1, B2)
+    print 'B1 outer B2: ', result.s
+    assert np.allclose(0, result.s)
 
 
 